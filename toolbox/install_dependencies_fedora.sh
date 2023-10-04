fdbc06668f30ae817e70f6e8a6ee59ed7ec6dfee
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2023-10-04 15:45:33 +0200

    Update order of installation of dependencies

--- toolbox/install_dependencies_fedora.sh
+++ toolbox/install_dependencies_fedora.sh
@@ -1,6 +1,9 @@
+# libcurl-devel must go before libcurl, because otherwise dnf might install
+# the 32-bit version of libcurl, which will not work with the ESO m4 macros.
 dnf install -y \
     wget gcc gcc-g++ automake autogen libtool gsl gsl-devel fftw fftw-devel \
     curl bzip2 less svn git which dnf-plugins-core cppcheck lcov valgrind \
-    erfa erfa-devel libcurl libcurl-devel \
+    erfa erfa-devel \
+    libcurl-devel libcurl \
     tmux ripgrep file \
     cfitsio wcslib cpl esorex cfitsio-devel wcslib-devel cpl-devel
