d36d5cf10cac13b110344e42e413d37d1a110f0c
Chi-Hung Yan <chyan@asiaa.sinica.edu.tw>
2024-06-19 17:35:37 +0200

    Classify the data

--- prototypes/workflows/metis/metis_dark_wkf.py
+++ prototypes/workflows/metis/metis_dark_wkf.py
@@ -3,11 +3,20 @@ from edps import task, data_source, classification_rule
 
 
 dark_class = classification_rule("DARK",{"instrume":"METIS", "dpr.catg": "CALIB", "dpr.type":"DARK",})
+detlin_class = classification_rule("DETLIN",{"instrume":"METIS", "dpr.catg": "CALIB", "dpr.type":"DETLIN",})
+lampflat_class = classification_rule("LAMP_FLAT",{"instrume":"METIS", "dpr.catg": "CALIB", "dpr.type":"FLAT,LAMP",})
+twlightflat_class = classification_rule("TWLIGHT_FLAT",{"instrume":"METIS", "dpr.catg": "CALIB", "dpr.type":"FLAT,TWILIGHT",})
+
+obj_class = classification_rule("OBJECT",{"instrume":"METIS", "dpr.catg": "SCIENCE", "dpr.type":"OBJECT",})
 
 #classification_rule("dark",{"instrume":"METIS", "dpr.catg": "CALIB", "dpr.type":"DARK",})
 # --- Data sources ---
 master_dark = (data_source()
             .with_classification_rule(dark_class)
+            .build())
+
+twlight_flat = (data_source()
+            .with_classification_rule(lampflat_class)
             .build())
 
 
