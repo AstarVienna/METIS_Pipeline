5ed8f7785ccf67afbaee71bec6dceb5b649911c4
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2024-10-16 21:40:15 +0200

    Add all recipes to metis_recipes.py

--- metisp/pyrecipes/metis_recipes.py
+++ metisp/pyrecipes/metis_recipes.py
@@ -1,7 +1,24 @@
-from pymetis.recipes.metis_det_lingain import MetisDetLinearGain
+from pymetis.recipes.metis_det_lingain import MetisDetLinGain
 from pymetis.recipes.metis_det_dark import MetisDetDark
-from pymetis.recipes.img.metis_lm_basic_reduction import MetisLmBasicReduction
+from pymetis.recipes.img.metis_lm_basic_reduce import MetisLmBasicReduce
+from pymetis.recipes.img.metis_lm_img_flat import MetisLmImgFlat
+from pymetis.recipes.img.metis_n_img_flat import MetisNImgFlat
 from pymetis.recipes.ifu.metis_ifu_distortion import MetisIfuDistortion
-from pymetis.recipes.ifu.metis_ifu_postprocess import MetisIfuCalibrate
-# TODO: Add all recipes
+from pymetis.recipes.ifu.metis_ifu_calibrate import MetisIfuCalibrate
+from pymetis.recipes.ifu.metis_ifu_postprocess import MetisIfuPostprocess
+from pymetis.recipes.ifu.metis_ifu_reduce import MetisIfuReduce
+from pymetis.recipes.ifu.metis_ifu_telluric import MetisIfuTelluric
+
+__all__ = [
+    MetisDetLinGain,
+    MetisDetDark,
+    MetisLmBasicReduce,
+    MetisLmImgFlat,
+    MetisNImgFlat,
+    MetisIfuDistortion,
+    MetisIfuCalibrate,
+    MetisIfuPostprocess,
+    MetisIfuReduce,
+    MetisIfuTelluric,
+]
 
