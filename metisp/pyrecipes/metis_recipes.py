939a659b88ef87d6e9d0f5be401fb533ee47aa9a
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2024-10-16 20:43:52 +0200

    Add files required for installation

---
+++ metisp/pyrecipes/metis_recipes.py
@@ -0,0 +1,7 @@
+from pymetis.recipes.metis_det_lingain import MetisDetLinearGain
+from pymetis.recipes.metis_det_dark import MetisDetDark
+from pymetis.recipes.img.metis_lm_basic_reduction import MetisLmBasicReduction
+from pymetis.recipes.ifu.metis_ifu_distortion import MetisIfuDistortion
+from pymetis.recipes.ifu.metis_ifu_postprocess import MetisIfuCalibrate
+# TODO: Add all recipes
+
