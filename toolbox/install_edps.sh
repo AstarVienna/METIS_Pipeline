42270d74f5263d27801d4ca37ce19e8e45cc6491
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2024-11-05 19:04:14 +0100

    set PYCPL_RECIPE_DIR in run_edps.yaml

--- toolbox/install_edps.sh
+++ toolbox/install_edps.sh
@@ -5,5 +5,4 @@ set -Eeuo pipefail
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
 # Run as normal user 'metis'
-export PYCPL_RECIPE_DIR="{script_dir}/../metisp/pyrecipes/"
 pip install --extra-index-url https://ftp.eso.org/pub/dfs/pipelines/libraries pycpl pyesorex edps adari_core
