20671fca08cbf49c32568cc4c8fcc4f7596057c7
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2024-11-05 11:08:27 +0100

    Update configuration to be used with github actions

--- toolbox/install_edps.sh
+++ toolbox/install_edps.sh
@@ -5,5 +5,5 @@ set -Eeuo pipefail
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
 # Run as normal user 'metis'
-export PYCPL_RECIPE_DIR=$HOME/metis_pipeline/lib/esopipes-plugins
+export PYCPL_RECIPE_DIR="{script_dir}/../metisp/pyrecipes/"
 pip install --extra-index-url https://ftp.eso.org/pub/dfs/pipelines/libraries pycpl pyesorex edps adari_core
