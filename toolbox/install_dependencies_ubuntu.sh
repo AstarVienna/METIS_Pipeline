c9d209286f37af28c9e7cf70a7ee8d1747d48a38
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2024-11-05 13:05:32 +0100

    Add install_dependencies_ubuntu.sh

---
+++ toolbox/install_dependencies_ubuntu.sh
@@ -0,0 +1,17 @@
+apt-get update
+apt-get install -y \
+    wget gcc  automake autogen libtool gsl-bin libgsl-dev libfftw3-bin libfftw3-dev fftw-dev \
+    curl bzip2 less subversion git which dnf-plugins-core cppcheck lcov valgrind \
+    zlib1g zlib1g-dev \
+    liberfa1 liberfa-dev \
+    libcurl4-openssl-dev libcurl4 \
+    tmux ripgrep file \
+    libcfitsio-bin libcfitsio-dev \
+    wcslib-dev wcslib-tools \
+    libcpl-dev \
+    python3-astropy python3-matplotlib python3-numpy \
+    perl cmake \
+    graphviz meld \
+    python3-pip python3-full \
+    python3-jupyter-core python3-jupyter-client python3-notebook \
+    emacs vim nano
