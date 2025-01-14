c9ecdfaeebb988e23d977d0ac2a7cebf3a5e3430
Chi-Hung Yan <chyan@asiaa.sinica.edu.tw>
2025-01-14 14:14:42 +0100

    First working version of metis_wkf.py

--- metisp/workflows/metis/metis_wkf.py
+++ metisp/workflows/metis/metis_wkf.py
@@ -1,15 +1,6 @@
-from edps import SCIENCE, QC1_CALIB, QC0, CALCHECKER
-from edps import task, ReportInput, subworkflow
+from edps import qc1calib, science, qc0
+from edps import task
 
-from .metis_lm_img_wkf import basic_reduction
+from .metis_datasources import *
+from .metis_lm_img_wkf import *
 
-@subworkflow("lm_img", "")
-def lm_img():
-    basic_reduction = (task('metis_lm_img_basic_reduce')
-            .with_recipe('metis_lm_img_basic_reduce')
-            .with_main_input(lm_raw_science)
-            .with_associated_input(lingain_task)
-            .with_associated_input(dark_task)
-            .with_associated_input(flat_task)
-            .with_meta_targets([QC1_CALIB])
-            .build())
