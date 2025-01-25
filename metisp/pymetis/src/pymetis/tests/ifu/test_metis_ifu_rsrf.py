7f9b1a310052300f79fa5e7702a8456e59ba907e
Martin Baláž <martin.balaz@trojsten.sk>
2025-01-25 21:07:19 +0000

    Tests for IFU RSRF

---
+++ metisp/pymetis/src/pymetis/tests/ifu/test_metis_ifu_rsrf.py
@@ -0,0 +1,46 @@
+"""
+This file is part of the METIS Pipeline.
+Copyright (C) 2024 European Southern Observatory
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+"""
+
+import pytest
+
+from pymetis.recipes.ifu.metis_ifu_rsrf import (MetisIfuRsrf as Recipe, MetisIfuRsrfImpl as Impl)
+from pymetis.tests.generic import BaseRecipeTest, BaseInputSetTest
+
+
+@pytest.fixture
+def name():
+    return 'metis_ifu_rsrf'
+
+
+@pytest.fixture
+def sof(name):
+    return f'{name}.sof'
+
+
+class TestRecipe(BaseRecipeTest):
+    """ A bunch of extremely simple and stupid test cases... just to see if it does something """
+    _recipe = Recipe
+
+    def test_pyesorex_runs_with_zero_exit_code_and_empty_stderr(self, name, sof, create_pyesorex):
+        super().test_pyesorex_runs_with_zero_exit_code_and_empty_stderr(name, sof, create_pyesorex)
+
+
+class TestInputSet(BaseInputSetTest):
+    impl = Impl
+    count = 1
