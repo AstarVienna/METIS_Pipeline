62a277dbc0345bec1605f9546ea531894e4d72cf
WK <kausch@lap13-c706.uibk.ac.at>
2025-02-03 16:31:44 +0100

    Last update before renaming branch

--- prototypes/workflows/metis/copy_files.sh
+++ prototypes/workflows/metis/copy_files.sh
@@ -1,7 +1,7 @@
 echo "Copying METIS EDPS files"
-cp -v /daten/ELT/METIS/devel/edps/workflows/metis/*.py .
-cp -v /daten/ELT/METIS/devel/edps/workflows/metis/*.sh .
-cp -v /daten/ELT/METIS/devel/edps/workflows/metis/*.png .
+rsync -av /daten/ELT/METIS/devel/edps/workflows/metis/*.py .
+rsync -av /daten/ELT/METIS/devel/edps/workflows/metis/*.sh .
+rsync -av /daten/ELT/METIS/devel/edps/workflows/metis/*.png .
 rm edps_activate.sh
 
 git add *.py *.sh README.md *.png
