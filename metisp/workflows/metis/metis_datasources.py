4563f35a06183c1e6f522dfe9fff214d3b82f5a3
eiseleb47 <benjamin.eisele0101@gmail.com>
2025-07-10 20:07:54 +0200

    Simplified imports for datasources

--- metisp/workflows/metis/metis_datasources.py
+++ metisp/workflows/metis/metis_datasources.py
@@ -2,6 +2,7 @@ from edps import data_source
 from edps.generator.time_range import *
 
 from .metis_classification import *
+from .metis_keywords import instrume, det_binx, det_biny, ins_mode
 
 # Convention for Data sources Association rule levels:
 # Each data source can have several match function which correspond to different
@@ -14,101 +15,101 @@ from .metis_classification import *
 #   level = 3 significant risk of bad quality results
 
 # standard matching keywords:
-setup = [metis_kwd.det_binx, metis_kwd.det_biny, metis_kwd.ins_mode]
-instrument_setup = [metis_kwd.instrume] + setup
+setup = [det_binx, det_biny, ins_mode]
+instrument_setup = [instrume] + setup
 
 
 # --- Data sources ---
 detlin_2rg_raw = (data_source()
             .with_classification_rule(detlin_2rg_raw_class)
-            .with_match_keywords(["instrume"])
+            .with_match_keywords([instrume])
             .build())
 
 raw_2rg_dark = (data_source()
             .with_classification_rule(rawdark_2rg_class)
-            .with_match_keywords(["instrume"])
+            .with_match_keywords([instrume])
             .build())
 
 lm_lamp_flat = (data_source()
             .with_classification_rule(lm_lampflat_class)
-            .with_match_keywords(["instrume"])
+            .with_match_keywords([instrume])
             .build())
 
 lm_distortion_raw = (data_source()
             .with_classification_rule(lm_distortion_raw_class)
-            .with_match_keywords(["instrume"])
+            .with_match_keywords([instrume])
             .build())
 
 lm_wcu_off_raw = (data_source()
             .with_classification_rule(lm_wcu_off_raw_class)
-            .with_match_keywords(["instrume"])
+            .with_match_keywords([instrume])
             .build())
 
 lm_raw_science = (data_source()
             .with_classification_rule(lm_raw_science_class)        
-            .with_match_keywords(["instrume"])
+            .with_match_keywords([instrume])
             .build())
 
 lm_raw_sky = (data_source()
             .with_classification_rule(lm_raw_sky_class)        
-            .with_match_keywords(["instrume"])
+            .with_match_keywords([instrume])
             .build())
 
 lm_raw_std = (data_source()
             .with_classification_rule(lm_raw_std_class)        
-            .with_match_keywords(["instrume"])
+            .with_match_keywords([instrume])
             .build())
 # ------- N BAND DATA SOURCES ---------
 
 detlin_geo_raw = (data_source()
             .with_classification_rule(detlin_geo_raw_class)
-            .with_match_keywords(["instrume"])
+            .with_match_keywords([instrume])
             .build())
 
 raw_geo_dark = (data_source()
             .with_classification_rule(rawdark_geo_class)
-            .with_match_keywords(["instrume"])
+            .with_match_keywords([instrume])
             .build())
 
 n_lamp_flat = (data_source()
             .with_classification_rule(n_lampflat_class)
-            .with_match_keywords(["instrume"])
+            .with_match_keywords([instrume])
             .build())
 
 n_distortion_raw = (data_source()
             .with_classification_rule(n_distortion_raw_class)
-            .with_match_keywords(["instrume"])
+            .with_match_keywords([instrume])
             .build())
 
 n_wcu_off_raw = (data_source()
             .with_classification_rule(n_wcu_off_raw_class)
-            .with_match_keywords(["instrume"])
+            .with_match_keywords([instrume])
             .build())
 
 n_raw_science = (data_source()
             .with_classification_rule(n_raw_science_class)        
-            .with_match_keywords(["instrume"])
+            .with_match_keywords([instrume])
             .build())
 
 n_raw_sky = (data_source()
             .with_classification_rule(n_raw_sky_class)        
-            .with_match_keywords(["instrume"])
+            .with_match_keywords([instrume])
             .build())
 
 n_raw_std = (data_source()
             .with_classification_rule(n_raw_std_class)        
-            .with_match_keywords(["instrume"])
+            .with_match_keywords([instrume])
             .build())
 
 
 fluxstd_catalog = (data_source()
                 .with_classification_rule(fluxstd_catalog_class)
-                .with_match_keywords(["instrume"])
+                .with_match_keywords([instrume])
                 .build())
 
 pinehole_tab = (data_source()
                 .with_classification_rule(pinhole_table_class)
-                .with_match_keywords(["instrume"])
+                .with_match_keywords([instrume])
                 .build())
 
 # --- IFU Data Sources ---
@@ -119,47 +120,47 @@ bad_pix_ifu_calib = (data_source()
 
 detlin_ifu_raw = (data_source()
               .with_classification_rule(detlin_ifu_class)
-              .with_match_keywords(["instrume"])
+              .with_match_keywords([instrume])
               .build())
 
 dark_ifu_raw = (data_source()
             .with_classification_rule(rawdark_ifu_class)
-            .with_match_keywords(["instrume"])
+            .with_match_keywords([instrume])
             .build())
 
 distortion_ifu_raw = (data_source()
                   .with_classification_rule(distortion_ifu_class)
-                  .with_match_keywords(["instrume"])
+                  .with_match_keywords([instrume])
                   .build())
 
 wave_ifu_raw = (data_source()
             .with_classification_rule(wave_ifu_class)
-            .with_match_keywords(["instrume"])
+            .with_match_keywords([instrume])
             .build())
 
 rsrf_ifu_raw = (data_source()
             .with_classification_rule(rsrf_ifu_class)
-            .with_match_keywords(["instrume"])
+            .with_match_keywords([instrume])
             .build())
 
 wcu_off_ifu_raw = (data_source()
                    .with_classification_rule(wcu_off_ifu_class)
-                   .with_match_keywords(["instrume"])
+                   .with_match_keywords([instrume])
                    .build())
 
 std_ifu_raw = (data_source()
            .with_classification_rule(std_ifu_class)
-           .with_match_keywords(["instrume"])
+           .with_match_keywords([instrume])
            .build())
 
 sky_ifu_raw = (data_source()
                .with_classification_rule(sky_ifu_class)
-               .with_match_keywords(["instrume"])
+               .with_match_keywords([instrume])
                .build())
 
 sci_ifu_raw = (data_source()
            .with_classification_rule(sci_ifu_class)
-           .with_match_keywords(["instrume"])
+           .with_match_keywords([instrume])
            .build())
 
 calib_persistence = (data_source()
