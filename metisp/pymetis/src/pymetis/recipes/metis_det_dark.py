8aba698c50b53190a699779626da10fe4d87ce54
JenniferKarr <jkarr@asiaa.sinica.edu.tw>
2025-08-01 13:48:50 +0800

    first pass at dark recipe
    - read in darks
    - coadd
    - calculate hot and cold pixels
    - create mask
    - calculate noise (sqrt of signal)
    - calculate QC parameters and update header

--- metisp/pymetis/src/pymetis/recipes/metis_det_dark.py
+++ metisp/pymetis/src/pymetis/recipes/metis_det_dark.py
@@ -31,6 +31,7 @@ from pymetis.classes.inputs import (RawInput, BadPixMapInput, PersistenceMapInpu
 from pymetis.classes.prefab import RawImageProcessor
 from pymetis.classes.recipes import MetisRecipe
 
+import numpy as np
 
 class MetisDetDarkImpl(RawImageProcessor, ABC):
     """
@@ -77,17 +78,145 @@ class MetisDetDarkImpl(RawImageProcessor, ABC):
     # See the documentation of the parent's `process` function for more details.
     # Feel free to define other functions to break up the algorithm into more manageable chunks
     # and call them from within `process` as needed.
+
+
     def process(self) -> set[DataItem]:
+
+        badBit = 2
+        hotBit = 4
+        coldBit = 8
+        
+        
         method = self.parameters["metis_det_dark.stacking.method"].value
-        Msg.info(self.__class__.__qualname__, f"Combining images using method {method!r}")
 
-        # TODO: preprocessing steps like persistence correction / nonlinearity (or not)
+        Msg.info(self.__class__.__qualname__, f"Pretending to Load DETLIN")
+        #linearity = cpl.core.Image.load(self.inputset.gain_map.frame.file, extension=0)
+        
+        Msg.info(self.__class__.__qualname__, f"Pretending to load GAIN_MAP")
+        #gain = cpl.core.Image.load(self.inputset.gain_map.frame.file, extension=0)
+        
+        Msg.info(self.__class__.__qualname__, f"Loading raw dark data")
         raw_images = self.inputset.load_raw_images()
-        combined_image = self.combine_images(raw_images, method)
+        
+        Msg.info(self.__class__.__qualname__, f"{len(raw_images)} Dark frames loaded")
+
+        ## TODO; if the DETLIN file exists, extract the bad pixel map, if not, initialize
+        ## a mask set to zeros.
+        
+        Msg.info(self.__class__.__qualname__, f"Pretending to copy mask from DETLIN product to product")
+        
+        bpMask = cpl.core.Image.zeros(2048, 2048, cpl.core.Type.INT)
+
+        Msg.info(self.__class__.__qualname__, f"Pretending to do persistence correction")
+        
+        Msg.info(self.__class__.__qualname__, f"Pretending to correct for non-linearity")
+
+        Msg.info(self.__class__.__qualname__, f"Combining images using method {method!r}")
+        combined_image = raw_images.collapse_create()
+        print(combined_image.get_median())
+
+        Msg.info(self.__class__.__qualname__, "Pretending to calculate noise")
+        
+        # simple calculation assuming noise is sqrt of image
+        
+        noise = cpl.core.Image(combined_image)
+        noise.copy_into(combined_image, 0, 0)
+        noise.power(0.5)
+        
+        Msg.info(self.__class__.__qualname__, "Calculate outlying pixels")
+        
+        darkRms = combined_image.get_stdev()
+        darkMedian = combined_image.get_median()
+        
+        # get masks from thresholds for bad, hot and cold pixels
+        # count the number of bad pixels in each, for later
+        # change to Image type from mask for later calculations
+        
+        mask = cpl.core.Mask.threshold_image(combined_image, darkMedian - 2*darkRms, darkMedian + 2*darkRms, 1)
+        qcnbad = mask.count()
+        mask = cpl.core.Image(mask,dtype = cpl.core.Type.INT)
+
+        maskCold = cpl.core.Mask.threshold_image(combined_image, 0, darkMedian - 2*darkRms, 1)
+        qcncold = maskCold.count()
+        maskCold = cpl.core.Image(maskCold,dtype = cpl.core.Type.INT)
+
+        maskHot = cpl.core.Mask.threshold_image(combined_image, 0, darkMedian + 2*darkRms, 1)
+        qcnhot = maskHot.count()
+        maskHot = cpl.core.Image(maskHot,dtype = cpl.core.Type.INT)
+
+        # multiple masks to the correct bitmask
+        mask.multiply_scalar(badBit)
+        maskCold.multiply_scalar(coldBit)
+        maskHot.multiply_scalar(hotBit)
+
+        # and update main mask
+        print(type(bpMask))
+        bpMask.add(mask)
+        bpMask.add(maskHot)
+        bpMask.add(maskCold)
+        
+        Msg.info(self.__class__.__qualname__, "Updating Mask; {mask.count()} pixels set as bad")
+        
+        ## how to copy mask into image? 
+        
+        Msg.info(self.__class__.__qualname__, "Actually Calculating QC Parameters")
+        
+        # calculate the stats in each individual image
+        medians = []
+        means = []
+        stdevs = []
+        mins = []
+        maxs = []
+        for im in raw_images:
+            print(im.get_median())
+            medians.append(im.get_median())
+            means.append(im.get_mean())
+            stdevs.append(im.get_stdev())
+            mins.append(im.get_min())
+            maxs.append(im.get_max())
+        
+        qcmed = combined_image.get_median()
+        qcmean = combined_image.get_mean()
+        qcrms = combined_image.get_stdev()
+        
+
+        qcncoadd = len(raw_images)
+        
+        qcmedmed = np.median(np.array(medians))
+        qcmedrms = np.median(np.array(stdevs))
+        qcmedmin = np.median(np.array(mins))
+        qcmedmax = np.median(np.array(maxs))
+        qcmedmean = np.median(np.array(means))
+            
+        
         header = cpl.core.PropertyList.load(self.inputset.raw.frameset[0].file, 0)
-
-        product = self.ProductMasterDark(header, combined_image)
-
+        Msg.info(self.__class__.__qualname__, "Appending QC Parameters to header")
+        
+        header.append(cpl.core.Property("QC DARK MEAN", cpl.core.Type.DOUBLE,
+                                        qcmean, "[ADU] mean value of master dark"))
+        header.append(cpl.core.Property("QC DARK MEDIAN", cpl.core.Type.DOUBLE,
+                                        qcmed, "[ADU] median value of master dark"))
+        header.append(cpl.core.Property("QC DARK RMS", cpl.core.Type.DOUBLE,
+                                        qcrms, "[ADU] rms value of master dark"))
+        header.append(cpl.core.Property("QC DARK NBADPIX", cpl.core.Type.DOUBLE,
+                                        qcnbad, "[ADU] number of bad pixels"))
+        header.append(cpl.core.Property("QC DARK NCOLDPIX", cpl.core.Type.DOUBLE,
+                                        qcncold, "[ADU] number of cold pixels"))
+        header.append(cpl.core.Property("QC DARK NHOTPIX", cpl.core.Type.DOUBLE,
+                                        qcnhot, "[ADU] number of hot pixels"))
+        header.append(cpl.core.Property("QC DARK MEDIAN MEAN", cpl.core.Type.DOUBLE,
+                                        qcmedmean, "[ADU] median value of mean values of individual input images"))
+        header.append(cpl.core.Property("QC DARK MEDIAN MEDIAN", cpl.core.Type.DOUBLE,
+                                        qcmedmed, "[ADU] median value of median values of individual input images"))
+        header.append(cpl.core.Property("QC DARK MEDIAN RMS", cpl.core.Type.DOUBLE,
+                                        qcmedrms, "[ADU] median value of RMS values of individual input images"))
+        header.append(cpl.core.Property("QC DARK MEDIAN MIN", cpl.core.Type.DOUBLE,
+                                        qcmedmin, "[ADU] median value of min values of individual input images"))
+        header.append(cpl.core.Property("QC DARK MEDIAN MAX", cpl.core.Type.DOUBLE,
+                                         qcmedmax, "[ADU] median value of max values of individual input images"))
+            
+        product = self.ProductMasterDark(header, combined_image,)
+        
         return {product}
 
 
