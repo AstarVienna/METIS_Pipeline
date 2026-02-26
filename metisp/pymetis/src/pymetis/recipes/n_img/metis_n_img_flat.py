0a520e164241737595b3c7622a696410722a7256
Chi-Hung Yan <chyan@asiaa.sinica.edu.tw>
2026-02-26 14:20:09 +0100

    Fix runner issue

--- metisp/pymetis/src/pymetis/recipes/n_img/metis_n_img_flat.py
+++ metisp/pymetis/src/pymetis/recipes/n_img/metis_n_img_flat.py
@@ -17,9 +17,11 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
 
+from abc import ABC
+
 from pyesorex.parameter import ParameterList, ParameterEnum
 
-from pymetis.classes.mixins import BandNMixin, DetectorGeoMixin
+from pymetis.classes.mixins import BandNMixin, DetectorGeoMixin, SourceLampMixin
 from pymetis.classes.recipes import MetisRecipe
 from pymetis.classes.prefab import MetisBaseImgFlatImpl
 
