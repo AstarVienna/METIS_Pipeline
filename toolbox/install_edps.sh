592c9baefeff709ee5a1c480adf670f4ac6a6405
janusbrink <janus.brink27@gmail.com>
2026-04-07 11:29:40 +0200

    Pin pybind version for PyCPL 1.0.3 compatibility
    Added a temporary fix to pin pybind version for compatibility.

--- toolbox/install_edps.sh
+++ toolbox/install_edps.sh
@@ -5,4 +5,8 @@ set -Eeuo pipefail
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
 # Run as normal user 'metis'
+
+# Temporary fix! Pin pybind version so that PyCPL 1.0.3 compiles successfully until PyCPL is updated
+pip install "pybind11<3.0"
+
 pip install --extra-index-url https://ftp.eso.org/pub/dfs/pipelines/libraries pycpl pyesorex edps adari_core
