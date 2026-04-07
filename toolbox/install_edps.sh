2cab45629a7b5a9ecebaf5fa67fb5a9ddb6264fd
janusbrink <janus.brink27@gmail.com>
2026-04-07 12:21:31 +0200

    Make sure PyCPL uses the pinned pybind11 version during build
    Updated pybind11 installation to version >=2.8,<3.0 and modified pycpl installation to use --no-build-isolation.

--- toolbox/install_edps.sh
+++ toolbox/install_edps.sh
@@ -7,6 +7,9 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 # Run as normal user 'metis'
 
 # Temporary fix! Pin pybind version so that PyCPL 1.0.3 compiles successfully until PyCPL is updated
-pip install "pybind11<3.0"
-
-pip install --extra-index-url https://ftp.eso.org/pub/dfs/pipelines/libraries pycpl pyesorex edps adari_core
+# Pre-install pybind11 < 3.0 to avoid incompatibility with pycpl 1.0.3,
+# then build pycpl with --no-build-isolation so it uses this version
+# instead of pip's isolated build environment picking up pybind11 3.x.
+pip install "pybind11>=2.8,<3.0"
+pip install --no-build-isolation --extra-index-url https://ftp.eso.org/pub/dfs/pipelines/libraries pycpl
+pip install --extra-index-url https://ftp.eso.org/pub/dfs/pipelines/libraries pyesorex edps adari_core
